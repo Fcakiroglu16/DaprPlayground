b4ea33c805ab28ca21315301053c0e9da5f50a24
fcakiroglu16 <f-cakiroglu@outlook.com>
2025-11-26 15:34:24 +0300

    Remove weather forecast endpoints and related code from both services

--- DaprOneService.API/Program.cs
+++ DaprOneService.API/Program.cs
@@ -22,14 +22,6 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-var summaries = new[] { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index => new WeatherForecast(DateOnly.FromDateTime(DateTime.Now.AddDays(index)), Random.Shared.Next(-20, 55), summaries[Random.Shared.Next(summaries.Length)])).ToArray();
-    return forecast;
-}).WithName("GetWeatherForecast");
-
 // Endpoint to create a user and publish event
 app.MapPost("/users", async (CreateUserRequest request, DaprClient daprClient) =>
 {
@@ -49,9 +41,5 @@ app.MapPost("/users", async (CreateUserRequest request, DaprClient daprClient) =
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
 
 record CreateUserRequest(string UserName, string Email);
--- DaprTwoService.API/Program.cs
+++ DaprTwoService.API/Program.cs
@@ -19,14 +19,6 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-var summaries = new[] { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index => new WeatherForecast(DateOnly.FromDateTime(DateTime.Now.AddDays(index)), Random.Shared.Next(-20, 55), summaries[Random.Shared.Next(summaries.Length)])).ToArray();
-    return forecast;
-}).WithName("GetWeatherForecast");
-
 // Subscribe to UserCreatedEvent
 app.MapPost("/user-created", [Topic("pubsub", "user-created")] async (UserCreatedEvent userEvent, ILogger<Program> logger) =>
 {
@@ -40,8 +32,3 @@ app.MapPost("/user-created", [Topic("pubsub", "user-created")] async (UserCreate
 }).WithTopic("pubsub", "user-created");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
